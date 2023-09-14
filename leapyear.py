68d032dfed77a9125e4e9e2b1ca818feccd1723e
Mayur Ramesh Kyatham <kmayur632003@gmail.com>
2023-09-14 19:39:19 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -2,6 +2,7 @@ year = int(input("Which year do you want to check? "))
 
 if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
     print("Leap year.")
+    
     if year % 4 == 0 and year % 100 != 0:
         print(f"Because {year} is cleanly divisible by 4 and not by 100.")
     elif year % 400 == 0:
