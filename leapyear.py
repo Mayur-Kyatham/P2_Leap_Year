db6226559444f189db636a9bdc2332566ad63b2e
Mayur Ramesh Kyatham <kmayur632003@gmail.com>
2024-04-28 17:31:48 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -8,7 +8,6 @@ if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
         
     elif year % 400 == 0:
         print(f"Because {year} is cleanly divisible by 400.")
-
 else:
     print("Normal year.")
     print(f"Because {year} is NOT divisible by 4 or is divisible by 100 but not by 400.")
